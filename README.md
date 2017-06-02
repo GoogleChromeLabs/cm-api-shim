3bcaae34dd824e138ec807e3955d4103e5c11412
Jan Wilken Dörrie <jdoerrie@google.com>
2017-06-02 14:44:09 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,66 @@
+## How to use
+Simply include `cm-api-shim.js` in your project, for example in the following
+way:
+```html
+<script src="https://raw.githubusercontent.com/GoogleChrome/cm-api-shim/master/cm-api-shim.js"></script>
+```
+
+## Supported Features
+The following features will be supported through the use of this library:
+
+### `navigator.credentials.preventSilentAccess`
+`navigator.credentials.requireUserMediation` has been renamed to
+`navigator.credentials.preventSilentAccess`. Through the use of this library
+developers can simply use `preventSilentAccess`, which will dispatch to
+`requireUserMediation` for older browsers.
+
+### A `mediation` enum argument to `navigator.credentials.get`
+The boolean `unmediated` flag argument to `navigator.credentials.get` has been
+replaced by a `mediation` enum, which takes the values `'silent'`, `'optional'`
+and `'required'`. `mediation: 'silent'` and `mediation: 'optional'` directly map
+to `unmediated: true` and `unmediated: false`. As before, `medation: 'optional'`
+is the default value if nothing else is specified.
+
+Note: `mediation: 'required'` is a completely novel option that can not be
+polyfilled in older browsers. In this case the polyfill falls back to
+`mediation: 'optional'` and warns the developers about doing so via a console
+message.
+
+### Asynchronous creation of `Credential` objects
+It is now possible to create `Credential` objects asynchronously using
+`navigator.credentials.create`. This returns a `Promise` which will resolve to a
+`Credential` object if successful.
+
+Currently, both `password` and `federated` are supported as keys, which will
+try to create a `PasswordCredential` or `FederatedCredential`.
+
+Example Use:
+```js
+let cred = await navigator.credentials.create({password: {
+  id: "Foo",
+  password: "Bar",
+}});
+```
+
+## Unsupported Features
+While the Credential Management API provides access to the `password` property
+of `PasswordCredential` for recent browser versions, this can not be polyfilled
+and is not supported for older browsers.
+
+For this use case it is suggested to detect whether access to passwords is
+possible or not and dispatch accordingly.
+
+Example Code:
+```js
+let cred = await navigator.credentials.get({password: true});
+if (PasswordCredential.prototype.password) {
+  let body = { id: cred.id, password: cred.password };
+  fetch('my_endpoint.html', {body : body}).then(result => {
+      // Handle server response.
+  });
+} else {
+  fetch('my_endpoint.html', {credentials: cred}).then(result => {
+      // Handle server response.
+  });
+}
+```
---
+++ cm-api-shim.js
@@ -0,0 +1,103 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This file includes polyfills for recent features added to the Credential
+// Management API.
+
+// Polyfill for navigator.credentials.preventSilentAccess.
+(function() {
+  // Return early if CM API is not available or preventSilentAccess is already
+  // present.
+  if (!navigator.credentials || navigator.credentials.preventSilentAccess)
+    return;
+
+  navigator.credentials.preventSilentAccess =
+      navigator.credentials.requireUserMediation;
+}());
+
+// Polyfill for navigator.credentials.get supporting a mediation enum.
+(function() {
+  // Return early if CM API is not available or support for the mediation enum
+  // is already present.
+  if (!navigator.credentials || PasswordCredential.prototype.password)
+    return;
+
+  var oldGet = navigator.credentials.get.bind(navigator.credentials);
+  navigator.credentials.get = function(options) {
+    // If mediation is specified, check whether it contains a valid value and
+    // raise a TypeError otherwise.
+    var validOptions = ['silent', 'optional', 'required'];
+    if (options['mediation'] &&
+        validOptions.indexOf(options['mediation']) === -1) {
+      throw new TypeError(
+          "The provided value '" + options['mediation'] +
+          "' is not a valid enum value.");
+    }
+
+    // mediation: 'required' can not be supported in older browsers, so fall
+    // back to 'optional' and issue a warning.
+    if (options['mediation'] === 'required') {
+      console.warn(
+          "mediation: 'required' can not be supported in older browsers, " +
+          "using mediation: 'optional' instead.");
+    }
+
+    options['unmediated'] = options['mediation'] === 'silent';
+    return oldGet(options);
+  }
+}());
+
+// Polyfill for navigator.credentials.create.
+(function() {
+  // Return early if CM API is not available or create is already present.
+  if (!navigator.credentials || navigator.credentials.create)
+    return;
+
+  navigator.credentials.create = function(options) {
+    var error_msg = "NotSupportedError: Only 'password' and 'federated'" +
+        'credential types are currently supported.';
+
+    // Check whether exactly one option is specified, reject the Promise
+    // otherwise.
+    return new Promise(function(resolve, reject) {
+      if (Object.keys(options).length !== 1) {
+        reject(error_msg);
+        return;
+      }
+
+      // Dispatch to the appropriate constructors for 'password' and
+      // 'federated'.
+      if ('password' in options) {
+        try {
+          resolve(new PasswordCredential(options['password']));
+        } catch (e) {
+          reject(e);
+        }
+        return;
+      }
+
+      if ('federated' in options) {
+        try {
+          resolve(new FederatedCredential(options['federated']));
+        } catch (e) {
+          reject(e);
+        }
+        return;
+      }
+
+      // Reject anything else.
+      reject(error_msg);
+    });
+  };
+}());
